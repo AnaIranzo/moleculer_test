98c41df6f93610bc15caba44e43e266607db5420
AnaIranzo <aeiranzom@gmail.com>
2023-07-04 22:03:23 +0200

    first structure

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// index.js
 const { ServiceBroker } = require("moleculer");
 const HTTPServer = require("moleculer-web");
+const { v4: uuidv4 } = require('uuid');
 
 // Create the broker for node-1
-// Define nodeID and set the communication bus
 const brokerNode1 = new ServiceBroker({
   nodeID: "node-1",
   transporter: "NATS"
@@ -11,46 +10,108 @@ const brokerNode1 = new ServiceBroker({
 
 // Create the "gateway" service
 brokerNode1.createService({
-  // Define service name
   name: "gateway",
-  // Load the HTTP server
   mixins: [HTTPServer],
 
   settings: {
     routes: [
       {
         aliases: {
-          // When the "GET /products" request is made the "listProducts" action of "products" service is executed
-          "GET /products": "products.listProducts"
+          "GET /tasks": "tasks.listTasks", // Get all tasks
+          "POST /task": "tasks.createTask", // Create a new task
+          "GET /lists": "lists.getLists", // Get all tasks
+          "POST /list": "lists.createList", // Create a new task
+       
         }
       }
     ]
   }
 });
 
-// Create the broker for node-2
-// Define nodeID and set the communication bus
+// Create the broker instance for node-2
 const brokerNode2 = new ServiceBroker({
   nodeID: "node-2",
   transporter: "NATS"
 });
 
-// Create the "products" service
+// Create an empty array to store todos
+const lists = [];
+
+// Create the "todos" service
 brokerNode2.createService({
-  // Define service name
-  name: "products",
+  name: "lists",
 
   actions: {
-    // Define service action that returns the available products
-    listProducts(ctx) {
-      return [
-        { name: "Apples", price: 5 },
-        { name: "Oranges", price: 3 },
-        { name: "Bananas", price: 2 }
-      ];
-    }
+    getLists() {
+      // Return the array of todos
+      return lists;
+    },
+
+    createList(ctx) {
+      // Create a new todo
+      const { listName } = ctx.params;
+      const newList = { id: generateListId(), listName};
+      // Save the new list to the array
+      lists.push(newList);
+      return newList;
+    },
+
+  //    {
+  //   "listName": "test",
+  //   
+  // }
+
+
+  }
+});
+
+// Create an empty array to store todos
+const tasks = [];
+
+const brokerNode3 = new ServiceBroker({
+  nodeID: "node-3",
+  transporter: "NATS"
+});
+
+brokerNode3.createService({
+  name: "tasks",
+
+  actions: {
+    listTasks() {
+      // Return the array of todos
+      return tasks;
+    },
+
+    createTask(ctx) {
+      // Create a new todo
+      const { text } = ctx.params;
+      const newTask = { id: generateTaskId(), text, completed: false };
+      // Save the new todo to the array
+      tasks.push(newTask);
+      return newTask;
+    },
+
+  //    {
+  //   "text": "test",
+  //   "completed": false
+  // }
+
+
   }
 });
 
 // Start both brokers
-Promise.all([brokerNode1.start(), brokerNode2.start()]);
+Promise.all([brokerNode1.start(), brokerNode2.start(), brokerNode3.start()]);
+
+// Helper function to generate a unique ID for a new todo
+function generateTaskId() {
+
+  const uniqueId = uuidv4();
+  return uniqueId;
+}
+
+function generateListId() {
+
+  const uniqueId = uuidv4();
+  return uniqueId;
+}
